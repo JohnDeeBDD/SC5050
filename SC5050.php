aa34667665c0515f713e8dbf25ed866ee892d076
John Dee <johndeebdd@gmail.com>
2018-02-27 14:30:16 -0800

    woocommerce template overide

--- SC5050.php
+++ SC5050.php
@@ -30,3 +30,41 @@ if ($AdminActionAddTickets->dateTimeListenerConditionalBoolean()){
 add_action( 'woocommerce_order_status_completed', array(new CustomerActionPurchaseTickets, 'doPurchaseTickets'));
 
 add_action('init', array(new SystemActionListenForDeclareWinner, 'listen'));
+
+function myplugin_plugin_path() {
+    
+    // gets the absolute path to this plugin directory
+    
+    return untrailingslashit( plugin_dir_path( __FILE__ ) );
+}
+add_filter( 'woocommerce_locate_template', '\SC5050\myplugin_woocommerce_locate_template', 10, 3 );
+
+function myplugin_woocommerce_locate_template( $template, $template_name, $template_path ) {
+    global $woocommerce;
+    //die('hi!');
+    $_template = $template;
+    
+    if ( ! $template_path ) $template_path = $woocommerce->template_url;
+    
+    $plugin_path  = \SC5050\myplugin_plugin_path() . '/woocommerce/';
+    
+    // Look within passed path within the theme - this is priority
+    $template = locate_template(
+        
+        array(
+            $template_path . $template_name,
+            $template_name
+        )
+        );
+    
+    // Modification: Get the template from this plugin, if it exists
+    if ( ! $template && file_exists( $plugin_path . $template_name ) )
+        $template = $plugin_path . $template_name;
+        
+        // Use default template
+        if ( ! $template )
+            $template = $_template;
+            
+            // Return what we found
+            return $template;
+}
